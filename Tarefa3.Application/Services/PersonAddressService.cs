aa0ff17f4831e51fd23557c76b8272ba45dd8457
LeoBigSad <85249134+LeoBigSad@users.noreply.github.com>
2024-11-28 15:40:57 -0300

    Add files via upload

---
+++ Tarefa3.Application/Services/PersonAddressService.cs
@@ -0,0 +1,32 @@
+﻿using RestSharp;
+using System.Linq;
+using System.Threading.Tasks;
+using Tarefa3.Domain.Models;
+
+public class PersonAddressService
+{
+    private const string ViaCepBaseUrl = "https://viacep.com.br/ws/";
+    private readonly List<Person> _people;
+
+    public async Task<PersonAddress> GetAddressByPersonIdAsync(int personId)
+    {
+        // Buscar a pessoa pelo ID
+        var person = _people.FirstOrDefault(p => p.Id == personId);
+        if (person == null)
+            throw new Exception("Usuário não encontrado.");
+
+        if (string.IsNullOrWhiteSpace(person.Cep))
+            throw new Exception("CEP do usuário está vazio.");
+
+        // Fazer a chamada para o ViaCEP
+        var client = new RestClient(ViaCepBaseUrl);
+        var request = new RestRequest($"{person.Cep}/json/", Method.Get);
+
+        var response = await client.ExecuteAsync<PersonAddress>(request);
+
+        if (!response.IsSuccessful || response.Data == null)
+            throw new Exception("Não foi possível obter o endereço no ViaCEP.");
+
+        return response.Data;
+    }
+}
---
+++ Tarefa3.Application/Services/PersonService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarefa3.Domain.Interfaces.Service;
+using Tarefa3.Domain.Models;
+
+namespace Tarefa3.Application.Services
+{
+    public class PersonService : IPersonService
+    {
+        public List<Person> GetAll()
+        {
+            return new List<Person>
+            {
+                new Person { Id = 1, Name = "Leonardo Tristao", Age = 19, Cep = "13024-050" },
+                new Person { Id = 2, Name = "Hyan Schibelsky", Age = 18, Cep = "10000-003"},
+                new Person { Id = 3, Name = "Kairos Murilo", Age = 18, Cep = "" },
+                new Person { Id = 4, Name = "Andre Barros", Age = 19}
+            };
+        }
+    }
+}
