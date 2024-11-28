ffe1bcf3c9809275a1b2554b7fcfa30065889f4d
LeoBigSad <85249134+LeoBigSad@users.noreply.github.com>
2024-11-28 15:36:16 -0300

    Add files via upload

---
+++ Tarefa3.Domain/Interfaces/Service/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarefa3.Domain.Models;
+
+namespace Tarefa3.Domain.Interfaces.Service
+{
+    public interface IPersonService
+    {
+        List<Person> GetAll();
+    }
+}
---
+++ Tarefa3.Domain/Models/PersonAddress.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarefa3.Domain.Models
+{
+    public class PersonAddress
+    {
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string complemento { get; set; }
+        public string unidade { get; set; }
+        public string bairro { get; set; }
+        public string localidade { get; set; }
+        public string uf { get; set; }
+        public string estado { get; set; }
+    }
+}
