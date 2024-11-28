e5fd5cef111089cf89322553f9b91e54ce90cc3e
LeoBigSad <85249134+LeoBigSad@users.noreply.github.com>
2024-11-28 19:12:49 -0300

    Add files via upload

---
+++ Tarefa3.Data.Rest/Repository/BaseRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace Tarefa3.Data.Rest.Repository
+{
+    public class BaseRepository
+    {
+        private readonly RestClient _client;
+
+        public BaseRepository(string baseUrl)
+        {
+            _client = new RestClient(baseUrl);
+        }
+
+        public async Task<T> GetAsync<T>(string endpoint) where T : new()
+        {
+            var request = new RestRequest(endpoint, Method.Get);
+            var response = await _client.ExecuteAsync<T>(request);
+
+            if (!response.IsSuccessful || response.Data == null)
+                throw new Exception($"Request failed: {response.ErrorMessage}");
+
+            return response.Data;
+        }
+    }
+}
---
+++ Tarefa3.Domain/Interfaces/Service/RickAndMortyService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarefa3.Domain.Models;
+using Tarefa3.Data.Rest.Repository;
+using System.Text.Json;
+
+namespace Tarefa3.Domain.Interfaces.Service
+{
+    public class RickAndMortyService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RickAndMortyService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<CharacterResponse> GetCharacterByIdAsync(int id)
+        {
+            var response = await _httpClient.GetAsync($"https://rickandmortyapi.com/api/character/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Erro ao chamar a API do Rick and Morty.");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            // Confirma o JSON recebido
+            Console.WriteLine(content);
+
+            return JsonSerializer.Deserialize<CharacterResponse>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+        }
+
+    }
+
+}
